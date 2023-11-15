eb6be289a169cc5cac7034991c36ad194d217ff6
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-15 13:36:32 +0600

    Local server commneted

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors({
     origin: [
-        'http://localhost:5173',
+        // 'http://localhost:5173',
         'https://cars-doctor-17a15.web.app',
         'https://cars-doctor-17a15.firebaseapp.com'
 
